50ccb9a2ebf77abbea4a85e252bed99af56d90cc
Anastasiia Rerikh <145387501+MeGrecha@users.noreply.github.com>
2025-05-12 10:58:45 +0200

    Add files via upload

--- user.ts
+++ user.ts
@@ -9,3 +9,12 @@ export type UserAccountInfo = {
   darkmode: boolean;
   high_contrast: boolean;
 };
+
+export namespace UserAccountInfo {
+  export function getPoints(user: UserAccountInfo) {
+    return {
+      discount_points: user.discount_points,
+      loyalty_points: user.loyalty_points
+    };
+  }
+}
